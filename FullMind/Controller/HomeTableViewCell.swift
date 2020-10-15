17e50c52140896060f0b0a619b2db8b9e0a4ba21
Patricia <patysiq@yahoo.com.br>
2020-10-15 09:06:38 -0300

    table

---
+++ FullMind/Controller/HomeTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  HomeTableViewCell.swift
+//  FullMind
+//
+//  Created by PATRICIA S SIQUEIRA on 14/10/20.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
+extension HomeTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath)
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 180.0, height: 180.0)
+    }
+
+}
