53f6580d7b83c1b4f7d4f3cf9683c461543e1b35
Patricia <patysiq@yahoo.com.br>
2020-10-16 13:15:19 -0300

    add category

--- FullMind/Controller/HomeTableViewCell.swift
+++ FullMind/Controller/HomeTableViewCell.swift
@@ -9,8 +9,9 @@ import UIKit
 
 class HomeTableViewCell: UITableViewCell {
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var titleTableCell: UILabel!
     var number = Int.random(in: 1...6)
-    var titleText: String = "Oi"
+    var textCollection: [String] = ["Oi", "Hello", "Hi"]
     override func awakeFromNib() {
         super.awakeFromNib()
         collectionView.delegate = self
@@ -20,23 +21,35 @@ class HomeTableViewCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
+    
+    //MARK: - Add Notes
+    @IBAction func addCollecttionCell(_ sender: Any) {
 
-}
+        self.collectionView.reloadData()
+        }
+    }
+
+//MARK: - CollectionView Delegate and Datasourse
+
 extension HomeTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return textCollection.count
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath) as? HomeCollectionViewCell else {
             fatalError("Unable create cell")
         }
         cell.imageBack.image = UIImage(named: "\(number)")
-        cell.titleCollectionCell.text = titleText
-         return cell
+        cell.titleCollectionCell.text = textCollection[indexPath.row]
+        return cell
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: 180.0, height: 180.0)
     }
-
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("collection: \(indexPath.row)")
+        print(textCollection[indexPath.row])
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
 }
---
+++ FullMind/Controller/NoteViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NoteViewController.swift
+//  FullMind
+//
+//  Created by PATRICIA S SIQUEIRA on 15/10/20.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
