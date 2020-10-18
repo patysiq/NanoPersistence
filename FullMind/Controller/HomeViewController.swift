4bab5dd46b97eab899a6d7d23a80fd44a38da577
Patricia <patysiq@yahoo.com.br>
2020-10-18 15:55:31 -0300

    add coreData

--- FullMind/Controller/HomeViewController.swift
+++ FullMind/Controller/HomeViewController.swift
@@ -5,17 +5,23 @@
 //  Created by PATRICIA S SIQUEIRA on 13/10/20.
 //
 import UIKit
+import CoreData
 
 class HomeViewController: UIViewController {
     @IBOutlet var blurView: UIVisualEffectView!
     @IBOutlet var popupView: UIView!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var note: UITextView!
-    var category: [String] = []
-    var textNote: [String] = []
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext // swiftlint:disable:this force_cast
+
+    var category = [Category]()
+    var noteText = [Note]()
     var number = Int.random(in: 1...6)
-    var indexCollectionSelected: Int = 0
-    
+    var selectedNote: Category? {
+        didSet {
+            loadNotes()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Set blur view
@@ -25,7 +31,10 @@ class HomeViewController: UIViewController {
         // Do any additional setup after loading the view.
         tableView.delegate = self
         tableView.dataSource = self
-    }
+        //loadCategories()
+        //loadNotes()
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+   }
 
 // MARK: - Add TablewViewCells - Category
 
@@ -33,7 +42,10 @@ class HomeViewController: UIViewController {
         var textField = UITextField()
         let alert = UIAlertController(title: "Add new category", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
-            self.category.append(textField.text ?? "")
+            let newCategory = Category(context: self.context)
+            newCategory.title = textField.text ?? ""
+            self.category.append(newCategory)
+            self.saveItems()
             self.tableView.reloadData()
         }
         alert.addTextField { (alertTextField) in
@@ -47,7 +59,15 @@ class HomeViewController: UIViewController {
     @IBAction func doneButton(_ sender: Any) {
         animateOut(desiredView: popupView)
         animateOut(desiredView: blurView)
-        textNote.append(note.text)
+        if selectedNote == nil {
+            let newNote = Note(context: context)
+            newNote.text = note.text
+            newNote.parentCategory = self.selectedNote
+            noteText.append(newNote)
+        } else {
+            category[tableView.indexPathForSelectedRow?.row ?? 0].parentNote?.text = note.text
+        }
+        saveItems()
         for cell in tableView.visibleCells {
             (cell as? HomeTableViewCell)?.collectionView.reloadData()
         }
@@ -78,8 +98,48 @@ class HomeViewController: UIViewController {
             desiredView.removeFromSuperview()
         }
     }
+    // MARK: - Model Manupulation Methods
+    func saveItems() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context \(error)")
+        }
+        for cell in tableView.visibleCells {
+            (cell as? HomeTableViewCell)?.collectionView.reloadData()
+        }
+        self.tableView.reloadData()
+    }
+    func loadNotes(with request: NSFetchRequest<Note> = Note.fetchRequest(), predicate: NSPredicate? = nil) {
+        let categoryPredicate = NSPredicate(format: "parentCategory.title MATCHES %@", selectedNote!.title!)
+        if let addtionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, addtionalPredicate])
+        } else {
+            request.predicate = categoryPredicate
+        }
+        do {
+            noteText = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+        for cell in tableView.visibleCells {
+            (cell as? HomeTableViewCell)?.collectionView.reloadData()
+        }
+        tableView.reloadData()
+    }
+    func loadCategories() {
+        let request : NSFetchRequest<Category> = Category.fetchRequest()
+        do {
+            category = try context.fetch(request)
+        } catch {
+            print("Error loading categories \(error)")
+        }
+        for cell in tableView.visibleCells {
+            (cell as? HomeTableViewCell)?.collectionView.reloadData()
+        }
+        tableView.reloadData()
+    }
 }
-
 // MARK: - TableView Delegate and Datasource Methods
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
@@ -88,18 +148,16 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? HomeTableViewCell else { fatalError("Unable create cell") }
-        cell.titleTableCell.text = category[indexPath.row]
+        cell.titleTableCell.text = category[indexPath.row].title
         cell.collectionView.delegate = self
         cell.collectionView.dataSource = self
         cell.delegate = self
-        //collectionViewHome = cell.collectionView
         return cell
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 230
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(indexPath.row)
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
@@ -108,21 +166,20 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
 
 extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return textNote.count
+        return noteText.count
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath) as? HomeCollectionViewCell else {
             fatalError("Unable create cell")
         }
         cell.imageBack.image = UIImage(named: "\(number)")
-        cell.titleCollectionCell.text = textNote[indexPath.row]
+        cell.titleCollectionCell.text = noteText[indexPath.row].text
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: 180.0, height: 180.0)
     }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        indexCollectionSelected = indexPath.row
         popupView.backgroundColor = UIColor(named: "\(number)")
         animateIn(desiredView: blurView)
         animateIn(desiredView: popupView)
